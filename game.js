941b97567b4802a0c0fd5cec2199fe2b21d49b7a
Bastien <bastien.nicoud@gmail.com>
2018-12-05 17:05:00 +0100

    Add circle class for adding circle to svg

--- game.js
+++ game.js
@@ -13,6 +13,7 @@ class Game {
   constructor (el, options) {
     this.el = el
     this.options = options
+    this.shapes = []
 
     // Create the SVG
     this.mountSVG()
@@ -36,4 +37,34 @@ class Game {
     svg.style = 'background-color: #a5b1c2;'
     return svg
   }
+
+  /**
+   * Ad a shape in the svg
+   */
+  addShape (shape) {
+    this.shapes.push(shape)
+  }
+}
+
+
+/**
+ * Circle class allows to create shapes in the svg
+ */
+class Circle {
+
+  constructor (posX, posY, radius, color) {
+    this.posX = posX
+    this.posY = posY
+    this.radius = radius
+    this.color = color
+
+    this.createElement()
+  }
+
+  createElement () {
+    this.el = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+    this.el.setAttributeNS(null, 'x', this.posX)
+    this.el.setAttributeNS(null, 'y', this.posY)
+  }
+
 }
