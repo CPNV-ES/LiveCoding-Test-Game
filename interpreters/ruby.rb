c8d10352c392dbe5a186f319633073ae14523400
Bastien <bastien.nicoud@gmail.com>
2018-12-13 10:33:54 +0100

    Little update on ruby interpreter type checks

--- interpreters/ruby.rb
+++ interpreters/ruby.rb
@@ -3,17 +3,20 @@ class Game
 
   # Move the circle on x axis
   def move_x x
-    x.is_a? Integer ? engine.send "game.moveX(#{x})" : raise "moveX only accepts integers"
+    raise "moveX only accepts integers" unless x.is_a? Integer
+    engine.send "game.moveX(#{x})"
   end
 
   # Move the circle on y axis
   def move_y y
-    y.is_a? Integer ? engine.send "game.moveX(#{y})" : raise "moveY only accepts integers"
+    raise "moveY only accepts integers" unless y.is_a? Integer
+    engine.send "game.moveX(#{y})"
   end
 
   # Move the circle on both x and y axis
   def move_x_y x, y
-    x.is_a? Integer && y.is_a? Integer ? engine.send "game.moveXY(#{x}, #{y})" : raise "moveXY only accepts integers"
+    raise "moveXY only accepts integers" unless x.is_a? Integer && y.is_a? Integer
+    engine.send "game.moveXY(#{x}, #{y})" :
   end
 
 end
