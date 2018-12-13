f810fce760d8944fd3c4418a9608f5a80991d3ba
Bastien <bastien.nicoud@gmail.com>
2018-12-13 10:15:06 +0100

    Adding type check ruby interpreter

--- interpreters/ruby.rb
+++ interpreters/ruby.rb
@@ -1,15 +1,19 @@
+# Game class allows the user to interact with the game
 class Game
 
+  # Move the circle on x axis
   def move_x x
-    engine.send "game.moveX(#{x})"
+    x.is_a? Integer ? engine.send "game.moveX(#{x})" : raise "moveX only accepts integers"
   end
 
+  # Move the circle on y axis
   def move_y y
-    engine.send "game.moveX(#{y})"
+    y.is_a? Integer ? engine.send "game.moveX(#{y})" : raise "moveY only accepts integers"
   end
 
+  # Move the circle on both x and y axis
   def move_x_y x, y
-    engine.send "game.moveXY(#{x}, #{y})"
+    x.is_a? Integer && y.is_a? Integer ? engine.send "game.moveXY(#{x}, #{y})" : raise "moveXY only accepts integers"
   end
 
 end
