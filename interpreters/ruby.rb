c758a283987606b0aeef06b8ccec2b85f11a619c
bastien <bastien.nicoud@gmail.com>
2019-03-08 12:03:29 +0100

    Ruby interpreter now use static engine methods

--- interpreters/ruby.rb
+++ interpreters/ruby.rb
@@ -2,21 +2,21 @@
 class Game
 
   # Move the circle on x axis
-  def move_x x
+  def self.move_x x
     raise "moveX only accepts integers" unless x.is_a? Integer
-    engine.send "game.moveX(#{x})"
+    Engine.send "game.moveX(#{x})"
   end
 
   # Move the circle on y axis
-  def move_y y
+  def self.move_y y
     raise "moveY only accepts integers" unless y.is_a? Integer
-    engine.send "game.moveX(#{y})"
+    Engine.send "game.moveX(#{y})"
   end
 
   # Move the circle on both x and y axis
-  def move_x_y x, y
+  def self.move_x_y x, y
     raise "moveXY only accepts integers" unless x.is_a? Integer && y.is_a? Integer
-    engine.send "game.moveXY(#{x}, #{y})" :
+    Engine.send "game.moveXY(#{x}, #{y})" :
   end
 
 end
